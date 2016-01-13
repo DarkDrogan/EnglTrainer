f951aaefcefb3d9f88f5df07f3b87097c4d1bb13
Max Avdeev <serafimmko@yandex.ru>
2016-01-13 04:16:10 +0300

    AdditionalVerbs checkstyled

--- src/main/java/cf/darkdrogan/apps/englTrainer/Execute/AdditionalVerbs.java
+++ src/main/java/cf/darkdrogan/apps/englTrainer/Execute/AdditionalVerbs.java
@@ -10,7 +10,8 @@ import cf.darkdrogan.apps.englTrainer.Variables;
 
 public final class AdditionalVerbs implements Variables {
     /**
-     * If this class is only a utility class, you should make the class final and define a private constructor:
+    * If this class is only a utility class,
+     * you should make the class final and define a private constructor.
      */
     private AdditionalVerbs() {
     }
@@ -20,57 +21,75 @@ public final class AdditionalVerbs implements Variables {
      */
     private static String word;
 
-    //getter for have
-    public static String getHave(int time, int prenoun, int form, int voice){
+    /**
+     * Getter for have.
+     * @param time  time
+     * @param prenoun   prenoun
+     * @param form  form
+     * @param voice voice
+     * @return  right form of have
+     */
+    public static String getHave(final int time, final int prenoun, final int form, final int voice) {
         //we use "==" because interface Variables have int constants of times, prenoun, and others. See Variables
-        if(time == PAST){
+        if (time == PAST) {
             word = "had";
         }
-        if(time == PRESENT){
+
+        if (time == PRESENT) {
             word = "have";
-            if(prenoun == HE || prenoun == SHE || prenoun == IT){
+            if (prenoun == HE || prenoun == SHE || prenoun == IT) {
                 word = "has";
             }
         }
+
         return word;
     }
 
-    //getter for be
-    public static String getBe(int time, int prenoun, int form, int voice){
-        if(time == PAST){
-            if(prenoun == I || prenoun == HE || prenoun == SHE || prenoun == IT){
-                if(form == ANSWER || form == ASK){
+    /**
+     * Getter for be.
+     * @param time  time
+     * @param prenoun   prenoun
+     * @param form  form
+     * @param voice voice
+     * @return  right form of be
+     */
+    public static String getBe(final int time, final int prenoun, final int form, final int voice) {
+        if (time == PAST) {
+            if (prenoun == I || prenoun == HE || prenoun == SHE || prenoun == IT) {
+                if (form == ANSWER || form == ASK) {
                     word = "was";
                 }
-                if(form == NEGATIVE){
+                if (form == NEGATIVE) {
                     word = "wasn't";
                 }
             }
-            if(prenoun == WE || prenoun == YOU || prenoun == THEY){
-                if(form == ANSWER || form == ASK){
+
+            if (prenoun == WE || prenoun == YOU || prenoun == THEY) {
+                if (form == ANSWER || form == ASK) {
                     word = "were";
                 }
-                if(form == NEGATIVE){
+                if (form == NEGATIVE) {
                     word = "weren't";
                 }
             }
         }
-        if(time == PRESENT){
-            if(prenoun == I){
+
+        if (time == PRESENT) {
+            if (prenoun == I) {
                 word = "am";
-                if(form == NEGATIVE){
+                if (form == NEGATIVE) {
                     word += " not";
                 }
             }
-            if(prenoun == WE || prenoun == YOU || prenoun == THEY){
+            if (prenoun == WE || prenoun == YOU || prenoun == THEY) {
                 word = "are";
-                if(form == NEGATIVE){
+                if (form == NEGATIVE) {
                     word += "n't";
                 }
             }
-            if(prenoun == HE || prenoun == SHE || prenoun == IT){
+            if (prenoun == HE || prenoun == SHE || prenoun == IT) {
                 word = "is";
-                if(form == NEGATIVE){
+                if (form == NEGATIVE) {
                     word += "n't";
                 }
             }
@@ -78,27 +97,37 @@ public final class AdditionalVerbs implements Variables {
         return word;
     }
 
-    //getter for Do
-    public static String getDo(int time, int prenoun, int form, int voice){
-        if(time == PRESENT){
-            if(form == ASK){
+    /**
+     * Getter for do.
+     * @param time  time
+     * @param prenoun   prenoun
+     * @param form  form
+     * @param voice voice
+     * @return  right form of do
+     */
+    public static String getDo(final int time, final int prenoun, final int form, final int voice) {
+        if (time == PRESENT) {
+            if (form == ASK) {
                 word = "Do";
-                if(prenoun == HE || prenoun == SHE || prenoun == IT){
+                if (prenoun == HE || prenoun == SHE || prenoun == IT) {
                     word = "Does";
                 }
             }
-            if(form == NEGATIVE){
+            if (form == NEGATIVE) {
                 word = "Don't";
-                if(prenoun == HE || prenoun == SHE || prenoun == IT){
+                if (prenoun == HE || prenoun == SHE || prenoun == IT) {
                     word = "Doesn't";
                 }
             }
         }
-        if(time == PAST){
+
+        if (time == PAST) {
             word = "Did";
-            if(form == NEGATIVE)
+            if (form == NEGATIVE) {
                 word = "Didn't";
+            }
         }
+
         return word;
     }
 }
